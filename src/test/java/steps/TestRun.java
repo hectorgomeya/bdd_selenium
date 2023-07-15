94b5391f3f160c4b10413bfa52d3d7359189831f
hectorgomeya <hectorgomeya@gmail.com>
2023-07-15 21:40:46 +0200

    acceder a la pestaña de cartera

--- src/test/java/steps/TestRun.java
+++ src/test/java/steps/TestRun.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/features/LoginTest.feature"},
+        features = {"src/test/resources/features/"},
         plugin = {"pretty", "html:target/cucumber-reports.html"})
 public class TestRun {
 }
